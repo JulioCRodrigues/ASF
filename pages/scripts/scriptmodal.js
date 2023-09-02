257ee255acc4d5eed8bdf789f8eaa37bab59c899
JULIO CESAR RODRIGUES <jurodrigues@brasilseg.com.br>
2023-09-01 23:28:46 -0300

    Frontend - Sistema de Suspeita de Fraude

---
+++ pages/scripts/scriptmodal.js
@@ -0,0 +1,11 @@
+function abrirModalAdicionarCliente(){
+    const modal = document.getElementById('janelaModal')
+
+    modal.classList.add('abrir')
+}
+
+function fecharModalAdicionarCliente(){
+    const modal = document.getElementById('janelaModal')
+
+    modal.classList.remove('abrir')
+}
